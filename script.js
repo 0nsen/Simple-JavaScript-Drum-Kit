90ce8f5a4f0418f1f6ef5c89df822d3951bdc856
Chau Phuong Gia <sphericalgia@gmail.com>
2020-03-13 13:10:23 +0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-window.addEventListener('keydown', e => {
-    console.log(e.keyCode);
+window.addEventListener('keydown', key => {
+    const audio = document.querySelector('audio[data-key="' + key.keyCode + '"]');
+    const key_to_animate = document.querySelector('div[data-key="' + key.keyCode + '"]');
+
+    // Animating the keys
+    key_to_animate.classList.add('playing');
+    // Removing the animation _after_ it's done playing
+    document.querySelector('div[data-key="' + key.keyCode + '"]').addEventListener('transitionend', function() {
+        key_to_animate.classList.remove('playing');
+    })
+
+    audio.currentTime = 0;
+    audio.play();
 });
